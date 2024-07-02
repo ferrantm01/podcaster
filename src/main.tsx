832ea20d3ec8982f4882b3e3601e55d35b2d7540
Ferran Torrent <ferrantorrent.tm@gmail.com>
2024-07-02 21:39:43 +0200

    - React Router implementation

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,33 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/main.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { PodcastsList } from "./pages/podcasts-list.tsx";
+import { PodcastDetails } from "./pages/podcast-details.tsx";
+import { EpisodeDetails } from "./pages/episode-details.tsx";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <PodcastsList />,
+      },
+      {
+        path: "/podcast/:podcastId",
+        element: <PodcastDetails />,
+      },
+      {
+        path: "/podcast/:podcastId/episode/:episodeId",
+        element: <EpisodeDetails />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
